932df94d8da7932a47bd28c7cd7e9216d31e26d9
xdimh <zzy7186@163.com>
2013-11-20 00:01:35 +0800

    add mongodb support

---
+++ models/db.js
@@ -0,0 +1,6 @@
+var settings = require('../setting');
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+
+module.exports = new Db(settings.db, new Server(settings.host,Connetion.DEFAULT_PORT,{}));
---
+++ setting.js
@@ -0,0 +1,5 @@
+module.exports = {
+	cookieSecret : 'microblogbyzzy',
+	db : 'microblog',
+	host : 'localhost'
+};
